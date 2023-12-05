679b8c123fc47baa64e95b027fc3148c675ad44d
mastershifu430 <micas.coder@gmail.com>
2023-12-05 13:44:21 +0000

    building database structure..

--- tour_management/models.py
+++ tour_management/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    customer_name = models.CharField(max_length=20)
+    customer_last_name = models.CharField(max_length=20)
+    contact_information = models.JSONField()
+    time = models.DateTimeField()
+    payer = models.CharField(
+        max_length=15,
+        choices=[("direct_client", "Direct Client"), ("agency", "Agency")],
+    )
+    number_of_guests = models.IntegerField()
+    tour_type = models.CharField(max_length=20)
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ("pending", "Pending"),
+            ("confirmed", "Confirmed"),
+            ("completed", "Completed"),
+            ("canceled", "Canceled"),
+            ("refunded", "Refunded"),
+        ],
+    )
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    contact_information = models.JSONField()
+    travel_preferences = models.JSONField()
+    travel_history = models.TextField()
+    preferred_language = models.CharField(max_length=255)
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=255)
+    contact_person_name = models.CharField(max_length=255)
+    contact_person_last_name = models.CharField(max_length=255)
+    email_address = models.EmailField()
+    contact_information = models.JSONField()
+    preferred_languages = models.CharField(max_length=255)
+
+
+class TourPackage(models.Model):
+    tour_name = models.CharField(max_length=255)
+    description = models.TextField()
+    itinerary = models.TextField()
+    duration = models.IntegerField()
+    included = models.TextField()
+    excluded = models.TextField()
+    remarks = models.TextField()
+    pricing = models.JSONField()  # Pricing structure based on tour package and season
+    supplier = models.ForeignKey("Supplier", on_delete=models.CASCADE)
+    availability = models.JSONField()  # Availability by date, tour type, and supplier
+
+
+class Season(models.Model):
+    season_name = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+
+class TourPackagePricing(models.Model):
+    tour_package = models.ForeignKey("TourPackage", on_delete=models.CASCADE)
+    season = models.ForeignKey("Season", on_delete=models.CASCADE)
+    price_tier = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    profit_margin_type = models.CharField(max_length=255)  # Percentage or fixed amount
+    profit_margin_value = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Tour(models.Model):
+    tour_name = models.CharField(max_length=50)
+    description = models.CharField()
+    itinerary = models.CharField()
--- tour_management/views.py
+++ tour_management/views.py
@@ -1,5 +1,68 @@
+import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Supplier, Booking
+
+
 def index(request):
     return HttpResponse("Hello! Welcome! Hello world!")
+
+
+def booking(request):
+    if request.method == "POST":
+        # Extract supplier ID from form data
+        supplier_id = request.POST["supplier"]
+
+        # Get the selected supplier
+        supplier = Supplier.objects.get(pk=supplier_id)
+
+        # Get tours for the selected supplier
+        tours = Tour.objects.filter(supplier=supplier)
+
+        # Filter tours based on booking date
+        booking_date = datetime.now()
+        available_tours = []
+        for tour in tours:
+            if (
+                tour.availability["available_seats"] >= 1
+                and booking_date >= tour.start_date
+                and booking_date <= tour.end_date
+            ):
+                available_tours.append(tour)
+
+        if request.POST["tour_package"]:
+            # Extract tour package ID from form data if selected
+            tour_package_id = request.POST["tour_package"]
+
+            # Get the selected tour package
+            tour_package = Tour.objects.get(pk=tour_package_id)
+
+            # Create a new booking instance
+            booking = Booking()
+            booking.customer_name = request.POST["customer_name"]
+            booking.customer_last_name = request.POST["customer_last_name"]
+            booking.payer = request.POST["payer"]
+            booking.tour = tour_package
+            booking.booking_date = datetime.now()
+            booking.number_of_guests = 1  # Assuming single booking
+            booking.status = "Pending"
+            booking.save()
+
+            # Send booking confirmation email
+            send_booking_confirmation_email(
+                booking.customer_name, booking.customer_last_name, booking
+            )
+
+            # Redirect to booking confirmation page
+            return render(request, "booking_confirmation.html", {"booking": booking})
+        else:
+            # Display booking form with available tours
+            return render(
+                request,
+                "booking.html",
+                {"suppliers": suppliers, "available_tours": available_tours},
+            )
+    else:
+        # Display booking form with suppliers
+        return render(request, "booking.html", {"suppliers": suppliers})
